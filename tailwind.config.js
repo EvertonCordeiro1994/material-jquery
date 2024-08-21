f2071722877fa8fb186eba3c672572d1ed1b82df
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-21 10:24:16 -0300

    inicio do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        color1: '#f4d464',
+        color2: '#e4c464',
+        color3: '#726130',
+        color4: '#ac944a',
+        color5: '#c0bec2',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
